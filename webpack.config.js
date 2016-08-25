f0d8dfc1cd94394eabb39e0bdaf430165a0f8426
martin byrne <marb61a@gmail.com>
2016-08-25 08:18:26 +0000

    added index.html and updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,34 @@ module.exports ={
         'script!jquery/dist/jquery.min.js',
         'script!foundation-sites/dist/foundation.min.js',
         './app/app.jsx'   
-    ]    
+    ],
+    externals : {
+        jquery: 'jQuery'
+    },
+    plugins : [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
+    output : {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve : {
+        
+    },
+    module : {
+        loaders : [
+            {
+                loader : 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]    
+    },
+    devtool : 'cheap-module-eval-source-map'
 };
