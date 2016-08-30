1e5375ae7c8cbb27864d0efeba0f32568cde31c7
martin byrne <marb61a@gmail.com>
2016-08-30 14:27:57 +0000

    updated weather

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -55,6 +55,31 @@ var Weather = React.createClass({
     
     render : function(){
         var {isLoading, temp, location, errorMessage} = this.state;
+        
+        function renderMessage(){
+            if(isLoading){
+                return <h3 className="text-center">Fetching weather...</h3>;
+            } else if (temp && location) {
+                return <WeatherMessage temp={temp} location={location}/>;    
+            }
+        }
+        
+        function renderError(){
+            if(typeof errorMessage === 'string'){
+                return (
+                    < ErrorModal message={errorMessage}/>    
+                );
+            }
+        }
+        
+        return(
+            <div>
+                <h1 className="text-center page-title">Get Weather</h1>
+                <WeatherForm onSearch={this.handleSearch}/>
+                {renderMessage()}
+                {renderError()}
+            </div>
+        );
     }
 });
 
